2eadf5d3752917ee9bc412c0b57d0f973b663c2a
HiDoYa <hiroyagojo@gmail.com>
2019-03-27 00:52:25 -0700

    Firefox runs headless

--- scrape.py
+++ scrape.py
@@ -3,24 +3,29 @@ from selenium import webdriver
 from scrapy.selector import Selector
 
 
-class QuotesSpider(scrapy.Spider):
+class LcsSpider(scrapy.Spider):
     name = 'lcsspider'
     start_urls = [
         'https://www.thescoreesports.com/lol/standings',
     ]
 
     def __init__ (self):
-        self.driver = webdriver.Firefox()
+        # Run firefox headless (invisible)
+        options = webdriver.firefox.options.Options()
+        options.add_argument('-headless')
+
+        self.driver = webdriver.Firefox(options=options)
 
     def parse(self, response):
         self.driver.get(response.url)
+
+        # Need to click since page is dynamic
         # next = self.driver.find_element_by_xpath('//a/[contains(@tabindex, "1")]')
         next = self.driver.find_element_by_css_selector('div[data-reactid="117"]')
         next.click()
         next = self.driver.find_element_by_css_selector('a[tabindex="1"]')
         next.click()
 
-        
         for quote in Selector(text=self.driver.page_source).css('tr[class^=StandingRow]'):
             yield {
                 'team': quote.css('td:nth-child(2) span::text').get(),
